d1285615813b1725cb889fa1754157435aef1415
Ganesh9986320435 <tganesh364@gmail.com>
2023-02-22 14:33:44 +0530

    hotelproject

---
+++ src/main/java/com/ty/springboot_hotel_project/dto/Room.java
@@ -0,0 +1,88 @@
+package com.ty.springboot_hotel_project.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Room {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String room_no;
+	private String room_type;
+	private String room_price;
+	private String no_of_beds;
+	private String max_no_of_people;
+	private Floor floor;
+
+	@ManyToOne
+	private Hotel hotel;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRoom_no() {
+		return room_no;
+	}
+
+	public void setRoom_no(String room_no) {
+		this.room_no = room_no;
+	}
+
+	public String getRoom_type() {
+		return room_type;
+	}
+
+	public void setRoom_type(String room_type) {
+		this.room_type = room_type;
+	}
+
+	public String getRoom_price() {
+		return room_price;
+	}
+
+	public void setRoom_price(String room_price) {
+		this.room_price = room_price;
+	}
+
+	public String getNo_of_beds() {
+		return no_of_beds;
+	}
+
+	public void setNo_of_beds(String no_of_beds) {
+		this.no_of_beds = no_of_beds;
+	}
+
+	public String getMax_no_of_people() {
+		return max_no_of_people;
+	}
+
+	public void setMax_no_of_people(String max_no_of_people) {
+		this.max_no_of_people = max_no_of_people;
+	}
+
+	public Floor getFloor() {
+		return floor;
+	}
+
+	public void setFloor(Floor floor) {
+		this.floor = floor;
+	}
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+	
+}
