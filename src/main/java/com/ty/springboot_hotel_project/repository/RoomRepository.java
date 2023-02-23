34486fb3dc2a50ab950e486164c41178af38dad2
Ganesh9986320435 <tganesh364@gmail.com>
2023-02-23 12:13:39 +0530

    completed repository layer

--- src/main/java/com/ty/springboot_hotel_project/repository/RoomRepository.java
+++ src/main/java/com/ty/springboot_hotel_project/repository/RoomRepository.java
@@ -1,9 +1,16 @@
 package com.ty.springboot_hotel_project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ty.springboot_hotel_project.dto.Room;
 
 public interface RoomRepository extends JpaRepository<Room, Integer>{
-
+	
+	@Query("select r from Room r where r.room_type=?1")
+	public Room getRoomByType(String room_type);
+	
+	@Query("select r from Room r where r.room_no=?1")
+	public Room getRoomByNo(String room_no);
+	
 }
